7ac1d4a2cfa217cdff84af88d87013b988c40297
kriscodes <kristian@kristiancodes.com>
2024-06-03 17:30:45 -0700

    sub header component and variable changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import ImageSlider from '../../components/ImageSlider';
+import Submenu from '../../components/Subheader';
 
 function Home() {
   return (
     <>
         <Header/>
         <ImageSlider/>
+        <Submenu/>
     </>
   );
 }
